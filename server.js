7624481ba1b63786806346d33c15a9b573f667f6
Yashwanth Sai <tanalayeshwanth@gmail.com>
2026-03-23 16:49:24 +0530

    solved page reloading error with /*

--- server.js
+++ server.js
@@ -30,10 +30,9 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "client/build")));
 
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
-
 
 
 app.use(ErrorHandler);
