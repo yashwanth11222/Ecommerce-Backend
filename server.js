0b7064f7c3f1f0554176634ad635f3f172687a41
Yashwanth Sai <tanalayeshwanth@gmail.com>
2026-03-23 17:05:11 +0530

    reload error fixed

--- server.js
+++ server.js
@@ -30,10 +30,9 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "client/build")));
 
 
-app.get('/:path(*)', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
-
 
 
 app.use(ErrorHandler);
